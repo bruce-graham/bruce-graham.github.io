715a3cead9b4d38427e4cca57f9104431d4eb255
Bruce <brucegraham11@gmail.com>
2017-09-28 15:26:26 -0700

    Remove comments, add 'throw err' and refactor from ES5 to ES6

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
-var token = '11393795.163594c.5d07d150cce04632823d439e2bc26a5a',
-    userid = 11393795,
-    num_photos = 18;
+const token = '11393795.163594c.5d07d150cce04632823d439e2bc26a5a';
+const userid = 11393795;
+const num_photos = 18;
 
 $.ajax({
-  url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
+  url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent',
   dataType: 'jsonp',
   type: 'GET',
   data: {access_token: token, count: num_photos},
   success: function (data) {
-    for(var x in data.data) {
-      $('.margin').append('<div class="s-12 m-6 l-4"><div class="image-with-hover-overlay image-hover-zoom margin-bottom"><a target="_blank" href="' + data.data[x].link +'"><div class="image-hover-overlay background-primary"><div class="image-hover-overlay-content text-center padding-2x"><p>Quote: ' + data.data[x].caption.text + '</p></div></div><img src="' + data.data[x].images.standard_resolution.url + '" alt="" title="Photography" /></div></a></div>');
-      // data.data[x].images.low_resolution.url - URL of image, 306х306
-      // data.data[x].images.thumbnail.url - URL of image 150х150
-      // data.data[x].images.standard_resolution.url - URL of image 612х612
-      // data.data[x].link - Instagram post URL
+    for(let x in data.data) {
+      $('.margin').append('<div class="s-12 m-6 l-4"><div class="image-with-hover-overlay image-hover-zoom margin-bottom"><a target="_blank" href="' + data.data[x].link +'"><div class="image-hover-overlay background-primary"><div class="image-hover-overlay-content text-center padding-2x"><p>CLICK TO OPEN IN INSTAGRAM<br><br>QUOTE ASSOC. w/ PHOTO: - ' + data.data[x].caption.text + '</p></div></div><img src="' + data.data[x].images.standard_resolution.url + '" alt="" title="Photography" /></div></a></div>');
     }
     $(".section.loading.background-white").removeClass('loading');
   },
   error: function(data){
-    console.log(data);
+    throw data;
   }
 });
 
